90dd94b050e71be5962a19f496e2185409d780bc
Júlio César Rodrigues Piau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2022-08-15 22:40:02 -0300

    desafio-ignite
    Desafio Ignite

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,20 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    try {
+      const users = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
+
+      return response.json(users);
+    } catch (err) {
+      if (err.message === "User not found")
+        return response.status(404).json({ error: err.message });
+
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
